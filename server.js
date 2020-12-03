03b1adde046e85e53d7d76389104a3a4cec19c1c
Bertrand Shao <shaob@g.ucla.edu>
2020-12-03 02:33:05 -0800

    feat: finish stripe project

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 const { static } = require('express');
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -11,8 +12,10 @@ console.log(stripeSecretKey)
 const express = require('express')
 const app = express()
 const fs = require('fs')
+const stripe = require('stripe')(stripeSecretKey)
 
 app.set('view engine', 'ejs')
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/store', function (req, res) {
@@ -23,6 +26,35 @@ app.get('/store', function (req, res) {
             res.render('store.ejs', {
                 stripePublicKey: stripePublicKey,
                 items: JSON.parse(data)
+            })
+        }
+    })
+})
+
+app.post('/purchase', function (req, res) {
+    fs.readFile('items.json', function (error, data) {
+        if (error) {
+            res.status(500).end()
+        } else {
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            let total = 0
+            req.body.items.forEach(function (item) {
+                const itemJson = itemsArray.find(function (i) {
+                    return i.id == item.id
+                })
+                total = total + itemJson.price * item.quantity
+            })
+            stripe.charges.create({
+                amount: total,
+                source: req.body.stripeTokenId,
+                currency: 'usd'
+            }).then(function () {
+                console.log('Charge Successful')
+                res.json({ message: 'Successfully purchased item' })
+            }).catch(function () {
+                console.log('Charge Fail')
+                res.status(500).end()
             })
         }
     })
